017aa844bd797f8bdfd7aa136f61530e456463db
Doug Banville <doug.banville@rte.ie>
2019-09-19 17:12:26 +0100

    search uses didUpdateAttrs

--- app/components/search-posts.js
+++ app/components/search-posts.js
@@ -1,14 +1,22 @@
 import Component from "@ember/component";
 import { task, timeout } from "ember-concurrency";
+import fade from "ember-animated/transitions/fade";
 
 export default Component.extend({
+  transition: fade,
+
   init() {
     this._super(...arguments);
     this.set("results", []);
   },
 
+  didUpdateAttrs() {
+    this.searchPosts.perform();
+  },
+
   searchPosts: task(function*() {
     yield timeout(250);
+    console.log(`run`);
     let searchUrl = `https://radio.rte.ie/radio1highlights/wp-json/wp/v2/posts?search=${this.query}&type=type=post`;
     let results = yield fetch(searchUrl)
       .then(r => {
